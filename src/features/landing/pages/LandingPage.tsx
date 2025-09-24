65473f998d63cda5ec3dbd7048d2646a06eb93ff
michellepoma <michellep132013@gmail.com>
2025-09-24 01:17:59 -0400

    Hero section

--- src/features/landing/pages/LandingPage.tsx
+++ src/features/landing/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "../../../components/Navbar";
+import { Hero } from "../../../components/Hero";
 
 export const LandingPage = () => {
   return (
@@ -8,17 +9,9 @@ export const LandingPage = () => {
       {/* Hero */}
       <section
         id="hero"
-        className="flex flex-col items-center justify-center flex-1 text-center px-6 min-h-screen"
       >
-        <h1 className="text-5xl font-bold text-azul1 mt-16">
-          Reserva tu cancha fácil y rápido ⚽
-        </h1>
-        <p className="mt-6 text-lg text-gris1 max-w-xl">
-          Con CanchaLic podrás encontrar, reservar y administrar tus espacios deportivos en segundos.
-        </p>
-        <button className="mt-8 bg-verde text-blanco px-8 py-3 rounded-lg font-semibold hover:opacity-90 transition">
-          Empieza ahora
-        </button>
+        {/* Hero */}
+      <Hero />
       </section>
 
       {/* Compañía */}
